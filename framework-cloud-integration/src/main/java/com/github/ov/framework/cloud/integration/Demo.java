0b25b49f874886b5e0803bcea4f40a5b55c0ef75
panyuanyuan <panyuanyuan@hujiang.com>
2019-01-11 11:54:58 +0800

    logback integration

---
+++ framework-cloud-integration/src/main/java/com/github/ov/framework/cloud/integration/Demo.java
@@ -0,0 +1,7 @@
+package com.github.ov.framework.cloud.integration;
+
+/**
+ * @author : JOSE 2019/1/11 11:27 AM
+ */
+public class Demo {
+}
--- framework-cloud-web/src/main/java/com/github/ov/framework/cloud/web/facade/ConfigFacade.java
+++ framework-cloud-web/src/main/java/com/github/ov/framework/cloud/web/facade/ConfigFacade.java
@@ -3,6 +3,7 @@ package com.github.ov.framework.cloud.web.facade;
 import com.github.ov.framework.cloud.config.biz.YmlConfig;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @RestController
 @RequestMapping(value = "/v1/config", produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
 @SuppressWarnings("unchecked")
 public class ConfigFacade {
 
@@ -26,6 +28,8 @@ public class ConfigFacade {
     @ApiOperation("测试配置文件")
     @GetMapping(value = "/query")
     public String getSwagger() {
+        log.info("你好");
+        log.error("测试");
         return ymlConfig.getSwagger();
     }
 
